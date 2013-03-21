8b6a4707a763efa6a7fa1bc45b636a954c1b4f71
Steven O'Brien <steve@newicon.net>
2013-03-21 17:41:38 +0000

    addig a new contact

--- angular/app/js/controllers.js
+++ angular/app/js/controllers.js
@@ -1,26 +1,32 @@
 'use strict';
 
 /* Controllers */
-
+var newContact = function(){};
+newContact.prototype.first_name = '';
+newContact.prototype.last_name = '';
 
 function ContactListCtrl($scope, $http) {
   $http.get('contacts/contacts.json').success(function(data) {
     $scope.contacts = data;
   });
+  
+  $scope.selContact = false;
   $scope.getContact = function(contact){
 	  $scope.selContact = contact;
   }
-  
   $scope.name = function(contact){
 	  if (contact.first_name == '' && contact.last_name)
 		  return 'No Name';
 	  return contact.first_name + ' ' + contact.last_name;
   } 
   $scope.editMode = false;
+  
   $scope.addContact = function(){
-	  $scope.selContact = newContact;
-	  $scope.contacts.push(newContact)
+	  var nc = {id:Math.random() ,first_name:'...', last_name:''};
+	  $scope.contacts.push(nc);
+	  $scope.selContact = nc;
 	  $scope.editMode = true;
+	  $('[ng-model="selContact.first_name"]').focus();
   }
   $scope.editContact = function(){
 	  $scope.editMode = true;
@@ -28,7 +34,10 @@ function ContactListCtrl($scope, $http) {
   $scope.editContactDone = function(){
 	  $scope.editMode = false;
   }
+ 
+  
   $scope.orderProp = 'first_name';
+ 
   
 }
 
